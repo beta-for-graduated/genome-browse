6c9cef9576006486645dc5faa73584358169b3c4
sqwww <2569362147@qq.com>
2020-11-26 22:47:56 +0800

    separate data folders by assembly name

--- scripts/deploy_jbrowse.js
+++ scripts/deploy_jbrowse.js
@@ -1,30 +1,67 @@
 const fs = require('fs');
 const process = require('process');
-const child_exec = require('child_process')
-const fileNames = fs.readdirSync('C:/Documents/Repos/genome-browse/data')
+const child_exec = require('child_process');
 
-// this script can accept an argument as the root path of data service
-const rootPath = process.argv[2] || 'http://localhost:8081';
+// Accept baseURL, localPath of genome data cmd arguments
+const DATA_BASE_URL = process.argv[2] || 'http://localhost:8081';
+const DATA_LOCAL_PATH = process.argv[3] || "C:/Documents/Repos/genome-browse/data";
 
-// firstly add assembly, ensuring that track can be add successfully
-fileNames.map(fileName => {
-  if(fileName.endsWith('fa')){
-    child_exec.execSync(
-      `jbrowse add-assembly ${rootPath}/${fileName} -f`
-    );
-    console.log(`Assembly ${fileName} has been added`)
+function getAssemblyNames () {
+  return fs.readdirSync(DATA_LOCAL_PATH);
+}
+
+function getFileNames (dirName) {
+  return fs.readdirSync(`${DATA_LOCAL_PATH}/${dirName}`);
+}
+
+function getFileUrl (dirName, fileName) {
+  return `${DATA_BASE_URL}/${dirName}/${fileName}`;
+}
+
+
+// Use jbrowse-cli to add assembly and track
+function addAssembly (fileUrl, assemblyName) {
+  child_exec.execSync(
+    `jbrowse add-assembly ${fileUrl} -f`
+  );
+  console.log(`Assembly ${fileUrl} has been added`)
+}
+
+function addFastaTrack (fileUrl, assemblyName) {
+  // TODO add .fa track
+}
+
+function addGffTrack (fileUrl, assemblyName) {
+  child_exec.execSync(
+    `jbrowse add-track ${fileUrl} -t AlignmentsTrack -a ${assemblyName} -f`
+  )
+  console.log(`Track ${fileUrl} has been added`)
+}
+
+
+// Deploy genome data into config.json
+function generateDeployer (extName, deployer) {
+  return function (assemblyName) {
+    getFileNames(assemblyName)
+    .filter(fileName => fileName.endsWith(extName))
+    .map(fileName => deployer(getFileUrl(assemblyName,fileName), assemblyName));
   }
-})
+}
 
-fileNames.map(fileName => {
-  if(fileName.endsWith('gff3.gz')){
-    child_exec.execSync(
-      `jbrowse add-track ${rootPath}/${fileName} -f`
-    )
-    console.log(`Track ${fileName} has been added`)
-  }
-})
+function deployAllData () {
+  // Assembly must be added before tracks
+  const deployers = [
+    generateDeployer('fa', addAssembly),
+    generateDeployer('gff3.gz', addGffTrack)
+  ];
+  getAssemblyNames()
+  .map(assemblyName => {
+    deployers.forEach(deploy => deploy(assemblyName));
+  })
+}
 
-// move config.json into 'jbrowse'
+
+// Generate config.json and move to 'jbrowse'
+deployAllData();
 fs.copyFileSync('C:/Documents/Repos/genome-browse/config.json', 'C:/Documents/Repos/genome-browse/jbrowse/config.json');
-fs.unlink('C:/Documents/Repos/genome-browse/config.json')
+fs.unlinkSync('C:/Documents/Repos/genome-browse/config.json');
