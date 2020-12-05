e4f4ea1d0d3565ce4344761366018f10d36e5e89
sqwww <2569362147@qq.com>
2020-12-05 21:45:01 +0800

    complete Download page with manifest.json

--- components/Download.js
+++ components/Download.js
@@ -1,5 +1,32 @@
-export default () => (
-  <div>
-    Blast
-  </div>
-)
+import { Tree } from 'antd'
+import manifest from '../manifest.json'  
+import {download} from '../styles/utils.module.css'
+
+const { DirectoryTree } = Tree;
+const treeData = manifest.data.download;
+
+export default function Download () {
+  const onSelect = (keys, { node }) => {
+    var a = document.createElement('a');
+    a.href = node.url;
+    a.download = node.title;
+    a.click();
+  };
+
+  const onExpand = () => {
+    console.log('Trigger Expand');
+  };
+
+  return (
+    <div
+    className={download}>
+      <DirectoryTree
+      onSelect={onSelect}
+      onExpand={onExpand}
+      treeData={treeData}
+      />
+    </div>
+    
+  );
+}
+
--- components/Jbrowse.js
+++ components/Jbrowse.js
@@ -1,12 +1,12 @@
 import { Fragment } from "react";
-import utilStyles from '../styles/utils.module.css'
+import {jbrowse} from '../styles/utils.module.css'
 
 
 export default function Jbrowse() {
   return (
     <Fragment>
       <iframe 
-        className={utilStyles.jbrowse}
+        className={jbrowse}
         id="jbrowse" 
         width="100%"
         src="http://localhost:8080"
