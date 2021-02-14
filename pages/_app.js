fde228c886f817c59807b8d550c5299206857337
sqwwwok <sqwwwok@gamil.com>
2021-02-14 22:15:55 +0800

    add favicon and titles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
+import { Fragment } from 'react'
 import { Menu } from 'antd'
 import Link from 'next/link'
-import { Fragment } from 'react'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 import '../styles/index.css';
 import 'antd/dist/antd.css';
@@ -14,8 +16,20 @@ export default function App({ Component, pageProps }) {
     'Data Download': 'data-download',
     'Manual': 'manual'
   };
+  const router = useRouter();
+  function findNavigationName(path) {
+    path = path.slice(1);
+    return Object.keys(navigators).filter(
+      navName=>navigators[navName]===path
+    )[0] || '';
+  }
+  const currentNavigator = findNavigationName(router.pathname)
 
   return <Fragment>
+    <Head>
+      <title>{currentNavigator ? currentNavigator+' | ' : ''}Mangrove Genome Browser</title>
+      <link rel="icon" type="image/png" sizes="128x128" href="/mangrove-icon.png"></link>
+    </Head>
     <Menu
     className={navigation} 
     mode="horizontal">
